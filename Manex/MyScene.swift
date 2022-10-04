bf828ab20134ccbefe155e362ea68a58b718bf07
michaelyql <yql.michael@gmail.com>
2022-10-04 22:23:02 +0800

    Added a test custom scene

---
+++ Manex/MyScene.swift
@@ -0,0 +1,19 @@
+//
+//  MyScene.swift
+//  Manex
+//
+//  Created by michaelyangqianlong on 4/10/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class MyScene: SKScene {
+    
+    // this method is called when a scene is presented to a view
+    // so you should override this method for every scene 
+    override func didMove(to view: SKView) {
+        
+    }
+}
+
