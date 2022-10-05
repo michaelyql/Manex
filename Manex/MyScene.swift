1a8990026f4a184b439b3905db01b946f4a8ee96
michaelyql <yql.michael@gmail.com>
2022-10-05 23:24:33 +0800

    Added square node to MyScene.swift

--- Manex/MyScene.swift
+++ Manex/MyScene.swift
@@ -10,10 +10,18 @@ import GameplayKit
 
 class MyScene: SKScene {
     
+    private var square: SKShapeNode?
+    
     // this method is called when a scene is presented to a view
     // so you should override this method for every scene 
     override func didMove(to view: SKView) {
         
+        self.square = SKShapeNode(rect: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 1, height: 1)))
+        
+        if let square = self.square {
+            backgroundColor = SKColor.blue
+            addChild(square)
+        }
     }
 }
 
