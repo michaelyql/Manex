99b42a3da7340087c249c92f5ff34cecf40b506d
michaelyql <yql.michael@gmail.com>
2022-10-17 00:29:54 +0800

    Added SideMenu package dependency and added side menu navigation controller

--- Manex/GameViewController.swift
+++ Manex/GameViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
+import SideMenu
 
 class GameViewController: UIViewController {
     
+    private var sideMenu = SideMenuNavigationController(rootViewController: MenuViewController(with: ["Formations", "Manex Board", "Feedback"]))
     @IBOutlet weak var addShipButton: UIButton!
     @IBOutlet weak var removeShipButton: UIButton!
     var scene: GameScene? = nil
@@ -18,6 +19,9 @@ class GameViewController: UIViewController {
     // Perform a one-time instantiation and initialization of the VC's view's contents
     override func viewDidLoad() {
         super.viewDidLoad()
+        sideMenu.leftSide = true
+        SideMenuManager.default.leftMenuNavigationController = sideMenu
+        SideMenuManager.default.addPanGestureToPresent(toView: view)
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
@@ -41,6 +45,10 @@ class GameViewController: UIViewController {
         }
     }
     
+    @IBAction func didTapMenuButton() {
+        present(sideMenu, animated: true)
+    }
+    
     @IBAction func addShip(_ sender: UIButton) {
         scene?.addShipToColummn()
     }
@@ -59,5 +67,41 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+}
+
+class MenuViewController: UITableViewController {
+    
+    private let menuItems: [String]
+    
+    init(with menuItems: [String]) {
+        self.menuItems = menuItems
+        super.init(nibName: nil, bundle: nil)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // Table
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Relay to delegate about menu item selection
+        
     }
 }
